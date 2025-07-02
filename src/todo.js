006a396d5063f3367301056a6b46b001cfc5d3fe
manulzvz13 <manulzvz@gmail.com>
2025-07-02 15:09:59 -0600

    feat: Add Todo obj to todo.js

--- src/todo.js
+++ src/todo.js
@@ -1,1 +1,34 @@
-//To Do obj: title, notes, dueDate, priority, checklist, completed
+export default class Todo {
+  constructor(title, notes = '', dueDate = null, priority = 'normal', checklist = [], completed = false) {
+    this.title = title; // String: The title of the todo item
+    this.notes = notes; // String: Additional notes
+    this.dueDate = dueDate; // String or Date: Due date (e.g. '2025-07-02')
+    this.priority = priority; // String: 'low', 'normal', 'high', etc.
+    this.checklist = checklist; // Array of { item: string, done: boolean }
+    this.completed = completed; // Boolean: true if the todo is completed
+  }
+
+  // Mark the todo as completed
+  markCompleted() {
+    this.completed = true;
+  }
+
+  // Mark the todo as not completed
+  markIncomplete() {
+    this.completed = false;
+  }
+
+  // Add an item to the checklist
+  addChecklistItem(item) {
+    this.checklist.push({ item, done: false });
+  }
+
+  // Toggle a checklist item's done status by index
+  toggleChecklistItem(index) {
+    if (this.checklist[index]) {
+      this.checklist[index].done = !this.checklist[index].done;
+    }
+  }
+}
+
+
