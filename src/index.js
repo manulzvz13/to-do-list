a7ce1df6fc2167d966a89e53b6882ca6edb4bb3e
manulzvz13 <manulzvz@gmail.com>
2025-07-02 15:15:31 -0600

    feat: Add Project class in project.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log("hello");
+import Todo from "./todo";
+import Project from "./project";
--- src/project.js
+++ src/project.js
@@ -1,1 +1,35 @@
-// Project obj: name, arr of To Do's
+import Todo from './todo.js';
+
+export default class Project {
+  constructor(name) {
+    this.name = name; // String: The name of the project
+    this.todos = [];  // Array of Todo objects
+  }
+
+  // Add a new Todo to the project
+  addTodo(todo) {
+    if (todo instanceof Todo) {
+      this.todos.push(todo);
+    } else {
+      throw new Error('addTodo expects a Todo object');
+    }
+  }
+
+  // Remove a Todo by its index
+  removeTodo(index) {
+    if (index >= 0 && index < this.todos.length) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  // Get all todos (optionally filter by completed status)
+  getTodos(completed = null) {
+    if (completed === null) {
+      return this.todos;
+    }
+    return this.todos.filter(todo => todo.completed === completed);
+  }
+}
+
+
+
